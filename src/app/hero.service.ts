eba9c4ddd7bb14fe07ae939bb1fc2997195fd8b1
Namae <namaeconde@gmail.com>
2022-08-25 23:11:52 +0800

    Add addHero functionality

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -41,6 +41,13 @@ export class HeroService {
     );
   }
 
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
   }
