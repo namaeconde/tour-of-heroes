ac3c5aa998d58ea1aa801de20261cf0ab5aaaced
Namae <namaeconde@gmail.com>
2022-08-25 23:06:19 +0800

    Add update and save hero functionality

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -11,6 +11,9 @@ import { catchError, map, tap } from 'rxjs/operators';
 export class HeroService {
 
   private heroesUrl = 'api/heroes';  // URL to web api
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  }
 
   constructor(
     private http: HttpClient,
@@ -29,6 +32,13 @@ export class HeroService {
       tap(_ => this.log(`fetched hero id=${id}`)),
       catchError(this.handleError<Hero>(`getHero id=${id}`))
     )
+  }
+
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
   }
 
   private log(message: string) {
